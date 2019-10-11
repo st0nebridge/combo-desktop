f0ac7dec5a3449471974ebae2a64c7e90a0d7f0a
Brandon Stonebridge <brandon@techster.co.uk>
2019-10-11 11:43:35 +0100

    tweaks - fixed: URLs opening within Electron instead of browser - added new tray icons - updated README - added start in tray command line argument - removed 'WhatsApp is available for x link' - automatically run web update if available - close window on ESC - hidden menu bar (CTRL to show)

--- README.md
+++ README.md
@@ -1,24 +1,43 @@
-whatsapp
+whatsapp-desktop
 ========================
 WhatsApp Windows Desktop app with Notification Area Icon
 
-Electron window loads web.whatsapp.com and blinks with tray icon when new messages arrive
+Electron window loads web.whatsapp.com and blinks with tray icon when a new messages arrives.
 
-Can be minimzed to notification area via closing main window
+- Minimize the main window to the tray by closing it or by pressing ESC
+- Start minimized using **--tray** argument
+- Quit app use tray icon context menu
 
-To quit app use tray icon context menu
-
+_* requires [NPM](https://www.w3schools.com/nodejs/nodejs_npm.asp) / [Yarn](/yarnpkg.com) *_
 
 init
 ------------------------
-npm i
+`npm i`
 
+or
+
+`yarn install`
 
 run
 ------------------------
-npm start
+`npm start`
 
+or
+
+`yarn start`
+
+or
+
+`yarn tray` (minimized to tray)
 
 build
 ------------------------
-electron-builder ./
+`electron-builder ./`
+
+or
+
+`yarn dist`
+
+arguments
+------------------------
+- **--tray** - will start electron in the tray
--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const {app, BrowserWindow, Tray, Menu} = require('electron');
+const {app, BrowserWindow, Tray, Menu, dialog, remote } = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
+
 const path = require('path');
 
 const iconGreen = path.join(__dirname, 'icon.ico');
-const iconBlack = path.join(__dirname, 'black.ico');
+const iconTray = path.join(__dirname, 'white.ico');
+const iconNotif = path.join(__dirname, 'white_notif.ico');
 
 let win;
 let notifyTimer;
-let notifyIconIsBlack = false;
+let iconIsNotify = false;
+
+let cl_args = process.argv;
+let start_min = false;
+
+for(let ix = 1; ix < cl_args.length; ix++)
+{
+    if (cl_args[ix].toLowerCase() === "--tray")
+        start_min = true;
+}
 
 function start() {
     win = new BrowserWindow({
@@ -15,11 +27,22 @@ function start() {
         icon: iconGreen,
         title: 'WhatsApp',
         visible: true,
+        show: !start_min,
         webPreferences: {
             nodeIntegration: true,
             partition: "persist:main",
             webSecurity: false
-        }
+        },
+        autoHideMenuBar: true
+    });
+
+    electronLocalshortcut.register(win, 'Esc', () => {
+        win.hide();
+    });
+
+    win.webContents.on('new-window', function(event, url) {
+        event.preventDefault();
+        require('electron').shell.openExternal(url)
     });
 
     win.webContents.setUserAgent(win.webContents.getUserAgent().replace(/(Electron|Chrome)\/([0-9\.]+)\ /g, ""));
@@ -36,6 +59,32 @@ function start() {
             if (titleEl && titleEl.innerHTML.includes('Google Chrome 36+')) {
                 window.location.reload();
             }
+            
+            const onLoad = function()
+            {
+                var divs = document.querySelectorAll('#side > span > div > div > div > div > span > span');
+                for (var i = 0; i < divs.length; i++) {
+                    if (divs[i].innerText.toLowerCase().includes('click to update whatsapp'))
+                    {
+                        divs[i].click(); // update the whatsapp window if available
+                        break;
+                    }
+                }
+            };
+            
+            const hasLoaded = setInterval(function()
+            {
+                var anchors = document.querySelectorAll('div > a');
+                for (var i = 0; i < anchors.length; i++) {
+                    // if the download link exists, we can assume whatsapp has loaded
+                    if (anchors[i].attributes.href.value.includes('whatsapp.com/download'))
+                    {
+                        clearInterval(hasLoaded);
+                        anchors[i].parentNode.parentNode.parentNode.remove(); // remove download link
+                        onLoad();
+                    }
+                }
+            }, 1000);
         `);
     });
 
@@ -45,16 +94,16 @@ function start() {
 
     win.on('page-title-updated', function (e, title) {
         if (title !== 'WhatsApp') {
-            icon.setImage(iconBlack);
+            icon.setImage(iconNotif);
             if (notifyTimer) {
                 clearInterval(notifyTimer);
             }
             notifyTimer = setInterval(function () {
-                icon.setImage(notifyIconIsBlack ? iconBlack : iconGreen);
-                notifyIconIsBlack = !notifyIconIsBlack;
+                icon.setImage(iconIsNotify ? iconNotif : iconTray);
+                iconIsNotify = !iconIsNotify;
             }, 1000)
         } else {
-            icon.setImage(iconGreen);
+            icon.setImage(iconTray);
             if (notifyTimer) {
                 clearInterval(notifyTimer);
             }
@@ -77,25 +126,25 @@ function start() {
     });
 
     win.on('closed', function () {
-        app.quit()
+        app.quit();
     });
 
     const menu = Menu.buildFromTemplate([
         {
             label: 'WhatsApp',
             click: function () {
-                win.show()
+                win.show();
             }
         },
         {
             label: 'Exit',
             click: function () {
-                app.exit()
+                app.exit(0);
             }
         }
     ]);
 
-    const icon = new Tray(iconGreen);
+    const icon = new Tray(iconTray);
     icon.setToolTip('WhatsApp');
     icon.setContextMenu(menu);
     icon.on('double-click', function () {
